56b7402330903e213b61625b0c1be0678a2a322c
joohnes <kubapienkowski@gmail.com>
2020-05-08 16:19:56 +0200

    created coin toss chance program

---
+++ coin.py
@@ -0,0 +1,30 @@
+from random import choice
+
+
+def showIt():
+    showIt = input("want to see result of each flip? (y/n)")
+    if showIt not in ["y","n"]:
+        print("Use \'y\' or \'n\'")
+        showIt()
+    return showIt
+
+def main():
+    times = int(input("How many times you want to flip a coin? : "))
+    possibleOutputs = ['HEADS','TAILS']
+    tails = 0
+    heads = 0
+    showresults = showIt()
+
+    for x in range(times):
+        side = choice(possibleOutputs)
+        if side == "TAILS":
+            tails += 1
+        else:
+            heads += 1
+        if showresults == "y": print(side)
+        if tails == heads: print(f"At {x} flips, the number of heads and tail were equal at {heads} each")
+    print(f"TAILS = {tails}/{times} | % -> {(tails/times) * 100}%")
+    print(f"HEADS = {heads}/{times} | % -> {(heads/times) * 100}%")
+
+if __name__ == "__main__":
+    main()
