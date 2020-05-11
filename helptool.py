339aef6cfa159d1dd2846a25bc1d991f82b73abd
ramirez <ramirez@pop-os.localdomain>
2020-05-11 19:03:38 +0200

    cleaned up

--- helptool.py
+++ helptool.py
@@ -1,38 +1,18 @@
-import os
-
-DECISIONS = ['delete','move','copy']
+from os import remove, listdir
 
 def deleteFiles(path, selected, specifier):
     for i in selected:
-        os.remove(f"{PATH}/{i}")
-
-def getDecision():
-    message = '(q to quit) '
-    for x in DECISIONS:
-        message += x.upper() + ' | '
-    decision = input(message[:-2]).lower()
-    if (decision not in DECISIONS) and decision != 'q':
-        print("This is not a valid option.")
-        getDecision()
-    if decision == 'q':
-        exit(0)
-    return decision
+        remove(f"{PATH}/{i}")
 
 def main():
-    decision = getDecision()
     PATH = input("COPY FULL PATH: ")
-    dir = os.listdir(PATH)
+    dir = listdir(PATH)
     specifier = input("ENTER SPECIFIER: ")
     selectedFiles = []
     for x in dir:
         if char in x:
             selectedFiles.append(x)
-    if decision == 'delete':
-        deleteFiles(PATH, selectedFiles, specifier)
-    elif decision == 'move':
-        pass
-    elif decision == 'copy':
-        pass
+    deleteFiles(PATH, selectedFiles, specifier)
 
 if __name__ == '__main__':
     main()
