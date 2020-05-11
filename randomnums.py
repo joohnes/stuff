df4a9dbf2bd9c7b43384c8302853bb98d42f768b
ramirez <ramirez@pop-os.localdomain>
2020-05-11 14:42:39 +0200

    created new file

---
+++ randomnums.py
@@ -0,0 +1,9 @@
+from random import randint, choice
+from termcolor import cprint, colored
+# colors=['red','green','yellow','blue','magenta','cyan','white']
+colors = ['grey','red']
+
+string = ''
+for x in range(10000):
+    string += colored(randint(0,100),choice(colors))
+print(string)
