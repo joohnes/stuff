7d943b0e97d60fcbace4f0c58b7633fce00e695a
ramirez <ramirez@pop-os.localdomain>
2020-05-11 22:22:43 +0200

    upgraded to be modal

--- namer.py
+++ namer.py
@@ -1,7 +1,10 @@
-from os import rename, listdir, mkdir
+from os import listdir, mkdir
 from shutil import copyfile
 PATH = input("COPY PATH: ")
 PathToFile = input("Path to name file: ")
+howMany = int(input("How many photos do you want to assign to one title? "))
+specifier = input("What are specifiers? (separated by space)")
+specifiers = specifier.split()
 
 with open(PathToFile,"r") as f:
     data = f.readlines()
@@ -22,13 +25,18 @@ except FileExistsError:
     print("Directory \'copied\' already exists")
 
 files = sorted(listdir(PATH))
+extension = files[1].split(".")[1]
+def createNames():
+    counter = 0
+    while True:
+        yield f"{specifiers[counter]}"
+        counter += 1
+
 counter = 0
 for x in processed:
     name = "_".join(x)
-    for i in range(2):
-        if counter % 2 == 0:
-            copyfile(PATH + "/" + files[counter],f"{PATH}/copied/{name}_gora.jpeg")
-            counter += 1
-        else:
-            copyfile(PATH + "/" + files[counter],f"{PATH}/copied/{name}_dol.jpeg")
-            counter += 1
+    spec = createNames()
+    extension = files[counter].split('.')[1]
+    for i in range(howMany):
+        copyfile(PATH + "/" + files[counter],f"{PATH}/copied/{name}_{next(spec)}.{extension}")
+        counter += 1
