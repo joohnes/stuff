78dfe83656ebbb538b3baf9583d83bde22d165e1
ramirez <ramirez@pop-os.localdomain>
2020-05-12 10:56:11 +0200

    cleaned up

--- namer.py
+++ namer.py
@@ -1,42 +1,54 @@
 from os import listdir, mkdir
 from shutil import copyfile
-PATH = input("COPY PATH: ")
-PathToFile = input("Path to name file: ")
-howMany = int(input("How many photos do you want to assign to one title? "))
-specifier = input("What are specifiers? (separated by space)")
-specifiers = specifier.split()
+from termcolor import cprint
 
-with open(PathToFile,"r") as f:
-    data = f.readlines()
-data = [x.strip() for x in data]
-data = [x.split() for x in data]
-processed = []
-for x in data:
-    part = []
-    for i in x:
-        if i == '-':
-            break
-        part.append(i)
-    processed.append(part)
-
-try:
-    mkdir(PATH+'/copied')
-except FileExistsError:
-    print("Directory \'copied\' already exists")
-
-files = sorted(listdir(PATH))
-extension = files[1].split(".")[1]
-def createNames():
+def createNames(names):
     counter = 0
     while True:
-        yield f"{specifiers[counter]}"
+        yield f"{names[counter]}"
         counter += 1
 
-counter = 0
-for x in processed:
-    name = "_".join(x)
-    spec = createNames()
-    extension = files[counter].split('.')[1]
-    for i in range(howMany):
-        copyfile(PATH + "/" + files[counter],f"{PATH}/copied/{name}_{next(spec)}.{extension}")
-        counter += 1
+def main():
+    PATH = input("COPY PATH: ")
+    PathToFile = input("Path to name file: ")
+    howMany = int(input("How many photos do you want to assign to one title? "))
+    specifiers = input("What are specifiers? (separated by space)").split()
+
+    with open(PathToFile,"r") as f:
+        data = f.readlines()
+
+    data = [x.strip() for x in data]
+    data = [x.split() for x in data]
+
+    processed = []
+    for x in data:
+        part = []
+        for i in x:
+            if i == '-':
+                break
+            part.append(i)
+        processed.append(part)
+
+    try:
+        mkdir(PATH+'/copied')
+    except FileExistsError:
+        print("Directory \'copied\' already exists")
+
+    files = sorted(listdir(PATH))
+    extension = files[1].split(".")[1]
+    try:
+        counter = 0
+        for x in processed:
+            name = "_".join(x)
+            spec = createNames(specifiers)
+            extension = files[counter].split('.')[1]
+            for i in range(howMany):
+                copyfile(PATH + "/" + files[counter],f"{PATH}/copied/{name}_{next(spec)}.{extension}")
+                counter += 1
+    except FileExistsError as e:
+        cprint(f"Could not create new files!","red")
+        raise FileExistsError
+    cprint("Successfully created new files!", "green")
+
+if __name__ == "__main__":
+    main()
