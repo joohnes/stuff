6d7c6edf74eefe4a6b541ead26c45f2cfc1e377e
ramirez <ramirez@pop-os.localdomain>
2020-05-12 11:03:12 +0200

    cleaned up

--- helptool.py
+++ helptool.py
@@ -1,6 +1,7 @@
 from os import remove, listdir
+from termcolor import cprint
 
-def deleteFiles(path, selected, specifier):
+def deleteFiles(path, selected):
     for i in selected:
         remove(f"{PATH}/{i}")
 
@@ -8,11 +9,18 @@ def main():
     PATH = input("COPY FULL PATH: ")
     dir = listdir(PATH)
     specifier = input("ENTER SPECIFIER: ")
-    selectedFiles = []
-    for x in dir:
-        if char in x:
-            selectedFiles.append(x)
-    deleteFiles(PATH, selectedFiles, specifier)
+
+    try:
+        selectedFiles = []
+        for x in dir:
+            if specifier in x:
+                selectedFiles.append(x)
+
+        deleteFiles(PATH, selectedFiles)
+    except Exception as e:
+        cprint("Could not delete those files.", "red")
+        exit()
+    cprint(f"{len(selectedFiles)} deleted successfully", "green")
 
 if __name__ == '__main__':
     main()
